f9e0d083dac58f7bd9a0ef69d4771206c6fbd8ca
tanmay sherkar <sherkartanmay@gmail.com>
2026-09-07 16:16:40 +0530

    shopping

---
+++ characterfrequency.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class characterfrequency {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        HashMap <Character, Integer> frequency = new HashMap<>();
+
+        System.out.println("Enter a String: ");
+        String text = sc.nextLine();
+
+        for (char ch: text.toCharArray()){
+            if (frequency.containsKey(ch)) {
+                frequency.put(ch, frequency.get(ch) + 1);
+            }else {
+                frequency.put(ch, 1);
+            }
+        }
+
+        System.out.println("\nCharacter Frequency: ");
+
+        for (Character ch : frequency.keySet()) {
+            System.out.println(ch + "→" + frequency.get(ch));
+        }
+        sc.close();      
+    }
+}
